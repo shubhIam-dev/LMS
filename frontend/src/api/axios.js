123ec48b214e06f633e3f3aa1b4938b87921d387
adityaghate66-sketch <adityaghate.66@gmail.com>
2026-07-23 18:13:24 +0530

    changes done for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+console.log("Backend URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
